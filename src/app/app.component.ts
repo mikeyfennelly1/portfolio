12ddace95c878b93103716605f9cdd8366a2bdb9
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2023-11-26 13:16:36 +0000

    all of portfolio made until 'select work section'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { OneComponent } from './components/one/one.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, HeroComponent, NavbarComponent, OneComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
---
+++ src/app/components/one/one.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-one',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './one.component.html',
+  styleUrl: './one.component.sass'
+})
+export class OneComponent {
+
+}
