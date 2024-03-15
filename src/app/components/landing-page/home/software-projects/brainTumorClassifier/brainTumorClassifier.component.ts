a7beece86632f4e14ce034b499be77f4dbde1309
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2024-03-15 12:41:26 +0000

    brain tumor classifier added to projects

---
+++ src/app/components/landing-page/home/software-projects/brainTumorClassifier/brainTumorClassifier.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-brainTumorClassifier',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './brainTumorClassifier.component.html',
+  styleUrl: './brainTumorClassifier.component.sass'
+})
+export class BrainTumorClassifierComponent {
+
+}
--- src/app/components/landing-page/home/software-projects/one.component.ts
+++ src/app/components/landing-page/home/software-projects/one.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { CharlemagneDBComponent } from './charlemagne-db/charlemagne-db.component';
 import { PointsLabComponent } from '../../../landing-page/home/software-projects/points-lab/points-lab.component';
 import { MyportfolioComponent } from '../../../landing-page/home/software-projects/myportfolio/myportfolio.component';
+import { BrainTumorClassifierComponent } from './brainTumorClassifier/brainTumorClassifier.component';
 
 @Component({
   selector: 'app-one',
   standalone: true,
-  imports: [CommonModule, CharlemagneDBComponent, PointsLabComponent, MyportfolioComponent],
+  imports: [CommonModule, CharlemagneDBComponent, PointsLabComponent, MyportfolioComponent, BrainTumorClassifierComponent],
   templateUrl: './one.component.html',
   styleUrl: './one.component.sass'
 })
