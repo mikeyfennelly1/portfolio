f397d6b34fdabb49b447fe4fa49710312f5653a2
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2024-03-21 13:14:17 +0000

    table of contents added

---
+++ src/app/components/bookshelf/bookshelf-features/table-of-contents/table-of-contents.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-table-of-contents',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './table-of-contents.component.html',
+  styleUrl: './table-of-contents.component.sass'
+})
+export class TableOfContentsComponent {
+
+}
