12d2c7e81ff99e412b55c245d0f7415ac24d2e93
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2024-04-13 22:19:18 +0100

    sizing adjusted for portfolio-item component

--- src/app/components/landing-page/home/software-projects/one.component.ts
+++ src/app/components/landing-page/home/software-projects/one.component.ts
@@ -15,21 +15,5 @@ import { PortfolioItemComponent } from './portfolio-item/portfolio-item.componen
   styleUrl: './one.component.sass'
 })
 export class OneComponent {
-  itemTitle: string = "Brain Tumor Classifier";
-  itemDescription: string = "This project is an image classifier that uses a convolutional neural network to classify brain scans, predicting if patients have brain tumors.";
-  itemTechnologies: Array<itemTechnologiesObject> = [
-    { name: "Python", link: "https://www.python.org/" },
-    { name: "Tensorflow", link: "https://www.tensorflow.org/" },
-    { name: "Keras", link: "https://keras.io/" },
-    { name: "OpenCV", link: "https://opencv.org/" },
-    { name: "Matplotlib", link: "https://matplotlib.org/" },
-    { name: "Pandas", link: "https://pandas.pydata.org/" },
-  ];
-  linkToSourceCode: string = "ExampleSourceCode";
-  linkToVideo: string = "ExampleLinkToVideo";
-  pathToThumbnail: string = "../../../../../../assets/images/brainScanThumbnail.png"
-}
-interface itemTechnologiesObject {
-  name: string;
-  link: string
+
 }
